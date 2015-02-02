1d8d425fe049f34d07859d2f3e8d058265b261bd
William Holt <wtholt27@gmail.com>
2015-02-02 14:08:12 -0500

    finished up workflow states, need to figure out a way to refactor

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -2,4 +2,22 @@ module PatientsHelper
   def patient_workflow(patient_object)
     patient_object.workflow_state
   end
+
+  def workflow_state_helper_method(patient)
+  if @patient.workflow_state == "waiting_room"
+    "Patient is in the waiting room"
+  elsif @patient.workflow_state == "doctor"
+    "Patient is with the Doctor"
+  elsif @patient.workflow_state == "xray"
+    "Patient is in the examination room"
+  elsif @patient.workflow_state == "surgery"
+    "Patient is in surgery"
+  elsif @patient.workflow_state == "paying"
+    "Patient is paying"
+  elsif @patient.workflow_state == "leaving"
+    "Patient is being discharged"
+    
+  end
+    
+  end
 end
